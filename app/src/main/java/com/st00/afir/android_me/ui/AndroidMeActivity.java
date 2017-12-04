3c638811304fe04ef98c2bc709dd1a73a140fe69
zakafir <zak.afir@gmail.com>
2017-12-04 13:48:58 +0000

    Three fragment

---
+++ app/src/main/java/com/st00/afir/android_me/ui/AndroidMeActivity.java
@@ -0,0 +1,59 @@
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.st00.afir.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.st00.afir.android_me.R;
+import com.st00.afir.android_me.data.AndroidImageAssets;
+
+// This activity will display a custom Android image composed of three body parts: head, body, and legs
+public class AndroidMeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_me);
+
+        // Create a new head BodyPartFragment
+        BodyPartFragment headFragment = new BodyPartFragment();
+        BodyPartFragment bodyFragment = new BodyPartFragment();
+        BodyPartFragment legsFragment = new BodyPartFragment();
+
+        // Set the list of image id's for the head fragment and set the position to the second image in the list
+        headFragment.setImageIds(AndroidImageAssets.getHeads());
+        bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+        legsFragment.setImageIds(AndroidImageAssets.getLegs());
+
+        headFragment.setListIndex(10);
+        bodyFragment.setListIndex(10);
+        legsFragment.setListIndex(10);
+
+        // Add the fragment to its container using a FragmentManager and a Transaction
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction()
+                .add(R.id.head_container, headFragment)
+                .add(R.id.body_container, bodyFragment)
+                .add(R.id.leg_container, legsFragment)
+                .commit();
+
+    }
+}
